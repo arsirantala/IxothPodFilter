7af76b32adbf28595d757b15420ffbd617b048da
Arsi Rantala <arsi.rantala@kolumbus.fi>
2020-10-09 07:11:30 +0300

    Add scripts to manipulate the item.filter in the repository

---
+++ scripts/enable_gems.sh
@@ -0,0 +1,7 @@
+sed -i -e "s|^ItemDisplay\[gzv\]:$|//ItemDisplay\[gzv\]\:|g" ../item.filter
+sed -i -e "s|^ItemDisplay\[glw\]:$|//ItemDisplay\[glw\]\:|g" ../item.filter
+sed -i -e "s|^ItemDisplay\[glg\]:$|//ItemDisplay\[glg\]\:|g" ../item.filter
+sed -i -e "s|^ItemDisplay\[glr\]:$|//ItemDisplay\[glr\]\:|g" ../item.filter
+sed -i -e "s|^ItemDisplay\[glb\]:$|//ItemDisplay\[glb\]\:|g" ../item.filter
+sed -i -e "s|^ItemDisplay\[gly\]:$|//ItemDisplay\[gly\]\:|g" ../item.filter
+
---
+++ scripts/readme.txt
@@ -0,0 +1,5 @@
+This directory contains simple scripts which will alter the item.filter file content.
+
+enable_gems.sh: will enable the hiding of the perfect gems
+
+PS: you need to have sed installed (install git bash to windows)
